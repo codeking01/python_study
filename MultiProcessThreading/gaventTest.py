5925040005b592bdf00e1af6a950667a23edbf9c
codeking <codekingbear@gmail.com>
2023-04-17 21:35:57 +0800

    2023.4.17 master commit。使用ctypes去终止python的线程

--- MultiProcessThreading/gaventTest.py
+++ MultiProcessThreading/gaventTest.py
@@ -1,11 +1,14 @@
 # author: code_king
 # time: 2023/4/17 11:25
 # file: gaventTest.py
+
 import gevent as gevent
 from gevent import monkey
 
 # 打补丁，识别所有耗时操作，便于自动操作
 monkey.patch_all()
+
+
 def my_coroutine(x):
     print("f{x} start")
     gevent.sleep(0.2)
@@ -14,3 +17,19 @@ def my_coroutine(x):
 
 # 创建协程
 coroutines = [gevent.spawn(my_coroutine(i)) for i in range(3)]
+
+
+def test(a,b):
+    print(11)
+    print(f"a:{a}")
+    print(f"b:{b}")
+    gevent.sleep(5)
+    print(222)
+    gevent.sleep(20)
+    print(333)
+    print(a)
+
+
+g1 = gevent.spawn(test,1,55)
+gevent.joinall([g1],timeout=1)
+print(555)
--- MultiProcessThreading/multiThreadingTest.py
+++ MultiProcessThreading/multiThreadingTest.py
@@ -1,6 +1,7 @@
 # author: code_king
 # time: 2023/4/17 15:04
 # file: multiThreadingTest.py
+import ctypes
 from threading import Thread
 
 num = 0
@@ -17,11 +18,19 @@ def task2():
     for i in range(10000000):
         num -= 1
 
+
 # 开启多线程
-t1=Thread(target=task1)
-t2=Thread(target=task2)
+t1 = Thread(target=task1)
+t2 = Thread(target=task2)
 t1.start()
 t2.start()
 t1.join()
 t2.join()
-print("num:",num)
+print("num:", num)
+
+
+# python终止线程的方法
+def kill_thread(self, inner_work_thread):
+    # 终止线程
+    async_exc = ctypes.py_object(SystemExit)
+    ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(inner_work_thread.ident), async_exc)
